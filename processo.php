4ca90a969c647f8d74cbea8f96e60439c47db2d3
GuiAlexe <166525103+GuiAlexe@users.noreply.github.com>
2024-04-26 10:23:08 -0300

    Update processo.php

--- processo.php
+++ processo.php
@@ -2,13 +2,11 @@
 session_start();
 include_once("conexao.php");
 
-$nome_usuario = filter_input(INPUT_POST, 'nome_usuario', FILTER_SANITIZE_STRING);
+$nome_usuario = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
 $cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_NUMBER_INT);
 $endereco = filter_input(INPUT_POST, 'endereco', FILTER_SANITIZE_STRING);
 $telefone = filter_input(INPUT_POST, 'tele', FILTER_SANITIZE_NUMBER_INT);
-// $nascimento = filter_input(INPUT_POST, 'nascimento', FILTER_SANITIZE_STRING);
-$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
 $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
 
-$create_user = "INSERT INTO cadastro (nome_usuario, cpf, endereco, tele, nome, senha) VALUES ('$nome_usuario', '$cpf', '$enderco', '$telefone', '$nome', '$senha')";
-$created_user = mysqli_query($conn, $create_user);
+$create_user = "INSERT INTO carrinho (nome, cpf, endereco, tele, senha) VALUES ('$nome_usuario', '$cpf', '$endereco', '$telefone', '$senha')";
+$created_user = mysqli_query($conexao, $create_user);
